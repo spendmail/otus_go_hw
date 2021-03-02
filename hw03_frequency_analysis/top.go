d87a6b2bf69a2b925e863d16764513e5391eca96
spendmail <info@spendmail.ru>
2021-03-02 16:17:42 +0700

    added Top10 implementation, error returning and test

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"regexp"
+	"sort"
+)
+
+const sliceLen = 10
+
+var ErrTextShorterThenNeeded = fmt.Errorf(fmt.Sprintf("given text is shorter than %d unique words", sliceLen))
+
+var regexpSplitter = regexp.MustCompile(`\s+`)
+
+type wordCounterStruct struct {
+	Word    string
+	Counter int
+}
+
+func Top10(sourceText string) ([]string, error) {
+	// If text is empty, return empty slice
+	if sourceText == "" {
+		return []string{}, nil
+	}
+
+	resultSlice := make([]string, sliceLen)
+	var wordsSlice []string
+	wordsMap := make(map[string]int)
+	wordCounterSlice := make([]wordCounterStruct, 100)
+
+	// Split text to a separate words
+	wordsSlice = regexpSplitter.Split(sourceText, -1)
+
+	// Counting the number of words
+	for _, value := range wordsSlice {
+		wordsMap[value]++
+	}
+
+	// If given text is shorter than 10 unique words, return an error
+	if len(wordsMap) < sliceLen {
+		return []string{}, ErrTextShorterThenNeeded
+	}
+
+	// Write words to a slice of structs for sorting
+	for word, counter := range wordsMap {
+		wordCounterSlice = append(wordCounterSlice, wordCounterStruct{word, counter})
+	}
+
+	// Sorting the slice "lexicographically"
+	sort.Slice(wordCounterSlice, func(i, j int) bool {
+		if wordCounterSlice[i].Counter == wordCounterSlice[j].Counter {
+			return wordCounterSlice[i].Word < wordCounterSlice[j].Word
+		}
+		return wordCounterSlice[i].Counter > wordCounterSlice[j].Counter
+	})
+
+	// Taking first ${sliceLen} items
+	for i := 0; i < sliceLen; i++ {
+		resultSlice[i] = wordCounterSlice[i].Word
+	}
+
+	return resultSlice, nil
 }

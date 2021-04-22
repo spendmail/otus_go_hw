53a216bf54533c6955b4852f4d99d2ee2b21ec1c
spendmail <info@spendmail.ru>
2021-04-22 18:32:22 +0700

    fixed the usecase, if the input file ends without \n

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -24,30 +24,29 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	rd := bufio.NewReader(r)
 	result := make(DomainStat)
 	user := &User{}
-	var line []byte
-	var err error
 
 	for {
 		// Reads line in bytes.
-		line, err = rd.ReadBytes('\n')
+		line, readErr := rd.ReadBytes('\n')
 
-		// Break in case of the end of file.
-		if errors.Is(err, io.EOF) {
-			break
-		}
-
-		if err != nil {
-			return nil, err
+		// Returns error, if the error is not io.EOF.
+		if readErr != nil && !errors.Is(readErr, io.EOF) {
+			return nil, readErr
 		}
 
 		// Unmarshalling using easyJson generated unmarshaler.
-		if err = user.UnmarshalJSON(line); err != nil {
+		if err := user.UnmarshalJSON(line); err != nil {
 			return nil, err
 		}
 
 		// Collecting the number of domains.
 		if strings.Contains(user.Email, "."+domain) {
 			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
+		}
+
+		// Break in case of the end of file.
+		if errors.Is(readErr, io.EOF) {
+			break
 		}
 	}
 

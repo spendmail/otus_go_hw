57af9eb28a01e4dc2a6c3974f108d17bfce8f087
spendmail <info@spendmail.ru>
2021-04-23 12:59:23 +0700

    added arguments parsing

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,41 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	ErrParamsParsing = errors.New("unable to parse input arguments, watch the example:\n./go-telnet --timeout=10s 127.0.0.1 8000")
+	timeout          string
+	host             string
+	port             string
+)
+
+func parseParams() error {
+	flag.StringVar(&timeout, "timeout", "5s", "connection timeout")
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 2 {
+		return ErrParamsParsing
+	}
+
+	host = args[0]
+	port = args[1]
+
+	return nil
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	err := parseParams()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(timeout, host, port)
 }

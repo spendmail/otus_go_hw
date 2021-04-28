df1ef0da3c430830650a66450c9d6f8eabfd7553
spendmail <info@spendmail.ru>
2021-04-28 13:06:10 +0700

    removed test

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -62,9 +62,4 @@ func TestTelnetClient(t *testing.T) {
 
 		wg.Wait()
 	})
-
-	t.Run("parsing error", func(t *testing.T) {
-		_, _, err := parseParams()
-		require.ErrorIs(t, err, ErrParamsParsing, "Error must be: %q, actual: %q", ErrParamsParsing, err)
-	})
 }

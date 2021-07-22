44a1e3679e41cc8c290f16ab64e102f1d0c687a5
spendmail <info@spendmail.ru>
2021-07-22 20:16:54 +0700

    added migration

---
+++ hw12_13_14_15_calendar/migrations/20210722120225_init.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE SEQUENCE app_event_id_seq INCREMENT BY 1 MINVALUE 1 START 1;
+CREATE TABLE app_event
+(
+    id          INT                                                      NOT NULL,
+    title       VARCHAR(128)                                             NOT NULL,
+    begin_date  TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    end_date    TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
+    description TEXT                           DEFAULT NULL,
+    owner_id    INT                                                      NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE INDEX IDX_13EE8992166D1F9C ON app_event (id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP SEQUENCE app_event_id_seq CASCADE;
+DROP TABLE app_event;
+-- +goose StatementEnd

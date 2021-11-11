e728ea867594d7719cf356d8b6eb59cec3d58ab2
spendmail <info@spendmail.ru>
2021-11-11 16:35:46 +0700

    added integration tests stub

---
+++ hw12_13_14_15_calendar/test/http_test.go
@@ -0,0 +1,12 @@
+package scripts
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestHTTP(t *testing.T) {
+	t.Run("test event create", func(t *testing.T) {
+		require.Equal(t, true, true, "remove me after all")
+	})
+}
